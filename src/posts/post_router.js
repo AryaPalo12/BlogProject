58f117d6a41e2b0e2e5246aafd9c7d3db334120c
Arya Sena Aji <aryasa1997@gmail.com>
2022-08-07 09:46:48 +0700

    remove verification from get posts

--- src/posts/post_router.js
+++ src/posts/post_router.js
@@ -3,7 +3,7 @@ const verifyToken = require('../middleware/token.verification');
 const postControllers = require('./post_controller');
 const postRouter = express.Router();
 
-postRouter.get("/user/posts/:postId?",verifyToken,postControllers.getPost);
+postRouter.get("/user/posts/:postId?",postControllers.getPost);
 
 postRouter.post("/user/posts",verifyToken,postControllers.createPost)
 
